4e19a9794bcac8a19a4aba3fa3a8b7046e7215e8
ramune256 <shungo180620@outlook.jp>
2025-11-30 17:52:06 +0900

    fix name

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 21:10:07 by shunwata          #+#    #+#             */
-/*   Updated: 2025/11/30 17:42:46 by shunwata         ###   ########.fr       */
+/*   Updated: 2025/11/30 17:43:40 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,8 @@ int	main(int ac, char **av, char **ev)
 			tokenize(&heap);
 			printf("tokenized\n"); //FOR DEBUG
 			parse(&heap);
-			printf("tokenized\n"); //FOR DEBUG
+			printf("parsed\n"); //FOR DEBUG
 			execute(heap.ast, &heap);
-			printf("tokenized\n"); //FOR DEBUG
 		}
 		else
 			print_exit();
