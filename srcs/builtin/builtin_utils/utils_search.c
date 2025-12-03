510da689103d1fed18cf8c41440e006648f8631a
ramune256 <shungo180620@outlook.jp>
2025-12-03 16:37:18 +0900

    fix typo

--- srcs/builtin/builtin_utils/utils_search.c
+++ srcs/builtin/builtin_utils/utils_search.c
@@ -6,22 +6,22 @@
 /*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 18:15:32 by nmasuda           #+#    #+#             */
-/*   Updated: 2025/11/30 15:49:17 by shunwata         ###   ########.fr       */
+/*   Updated: 2025/12/03 16:35:27 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-char	*search_get_env(char **ev, char *serch)
+char	*search_get_env(char **ev, char *search)
 {
 	int		i;
 	size_t	len;
 
 	i = 0;
-	len = ft_strlen(serch);
+	len = ft_strlen(search);
 	while (ev[i])
 	{
-		if (!ft_strncmp(ev[i], serch, len))
+		if (!ft_strncmp(ev[i], search, len))
 		{
 			if (ev[i][len] == '=')
 				return (ev[i] + len + 1);
