9a816b8602d5a32f1ddb070a5ebb5a7c9aaaaf0f
nmasu <nanaho.masuda.19@gmail.com>
2025-12-10 17:57:01 +0900

    add:expander test

--- srcs/core_expander.c
+++ srcs/core_expander.c
@@ -1,86 +1,7 @@
 
 #include "minishell.h"
 
-char *input(char *old_str, int start, int len, char *val)
-{
-    int new_len = ft_strlen(old_str) - len + ft_strlen(val);
-    char *new_str = malloc(sizeof(char) * (new_len + 1));
-    
-    if (!new_str)
-        return (NULL);
-    ft_strlcpy(new_str, old_str, start + 1);
-    ft_strlcat(new_str, val, new_len + 1);
-    ft_strlcat(new_str, old_str + start + len, new_len + 1);
 
-    return (new_str);
-}
-
-char *tenkai(char *line,char **ev,int j,t_alloc *heap)
-{
-    while(j)
-        (void)(line++,j--);
-    line++;//$の分
-    if(!ft_strncmp(line,"?",1))
-        return (ft_itoa(heap->exit_status));
-    int ev_num = 0;
-    int cnt;
-    while(ev[ev_num])
-    {
-        cnt = 0;
-        while(ev[ev_num][cnt] != '=')
-            cnt++;
-        int ev_len =  cnt;
-        if(!ft_strncmp(line, ev[ev_num], ev_len)
-           && ev[ev_num][ev_len] == '='
-           && ft_strncmp(ev[ev_num], "_", 1)
-           && (line[ev_len] == '\0'
-              || !(ft_isalnum(line[ev_len]) || line[ev_len] == '_')))
-            return (ft_strdup(ev[ev_num] + ev_len + 1));
-        ev_num++;
-    }
-    return (ft_strdup(""));
-}
-
-void doller_check(char **argv,t_alloc *heap)
-{
-    int i = 0;
-    while (argv[i])
-    {
-        int j = 0;
-        int single_flag = 0;
-        while (argv[i][j])
-        {
-            if (argv[i][j] == '\'')
-                single_flag = !single_flag;
-
-            // シングルクォート外のみ処理
-            if (argv[i][j] == '$' && !single_flag)
-            {
-                char *new = tenkai(argv[i], heap->ev_clone, j, heap);
-                int len = 1 +ft_strlen(new);
-                int before = ft_strlen(argv[i]);
-
-                char *temp = input(argv[i], j, len, new);
-                free(argv[i]);
-                argv[i] = temp;
-
-                int after = ft_strlen(argv[i]);
-                j += (after - before);
-                free(new);
-            }
-            j++;
-        }
-        i++;
-    }
-}
-
-void check(t_cmd *ast,t_alloc *heap)
-{
-//   " ' をチェックしながら $探すチェック
-    doller_check(ast->argv,heap);
-
-      
-}
 
 void expander(t_cmd *ast, t_alloc *heap)
 {
@@ -99,36 +20,7 @@ void expander(t_cmd *ast, t_alloc *heap)
 
 /////////////////////////////
 
-//展開のアジャイル開発用
-
-
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-
-// char	*ft_substr(char const *s, unsigned int start, size_t len)
-// {
-// 	char	*substr;
-// 	size_t	st_len;
-
-// 	if (!s)
-// 		return (NULL);
-// 	if (len == 0)
-// 		return (strdup(""));
-// 	st_len = strlen(s);
-// 	if (start >= st_len)
-// 		return (strdup(""));
-// 	if (len > st_len - start)
-// 		len = st_len - start;
-// 	substr = (char *)malloc(sizeof(char) * (len + 1));
-// 	if (!substr)
-// 		return (NULL);
-// 	strlcpy(substr, s + start, len + 1);
-// 	return (substr);
-// }
-
-
-// char *tenkai(char *line,char **ev,char * exit_status,int j)
+// char *tenkai(char *line,char **ev,char * exit_status,int j,int *ev_len)
 // {
 //     int i = 1;
 //     int cnt = 0;
@@ -137,14 +29,13 @@ void expander(t_cmd *ast, t_alloc *heap)
 //         return (exit_status);
 //     int ev_num = 0;
 // 	line = ft_substr(line,j,line_len);
-// 	printf("%s\n",line);
 //     while(ev[ev_num])
 //     {
 // 		cnt = 0;
 //         while(ev[ev_num][cnt] != '=')
 //             cnt++;
-//         int ev_len = cnt;
-//         if(!strncmp(line,ev[ev_num],ev_len))
+//         *ev_len = cnt;
+//         if(!strncmp(line,ev[ev_num],*ev_len))
 //             return(ft_substr(ev[ev_num],cnt + 1,strlen(ev[ev_num])));
 //         ev_num++;
 //     }
@@ -154,27 +45,45 @@ void expander(t_cmd *ast, t_alloc *heap)
 // int main(int ac,char **av,char **ev)
 // {
 //     int i = 1;
-// 	int j = 0;
+// 	   int j = 0;
+// 	int ev_len = 0;
+// 	char *tenkai_no_nakami = NULL;
+// 	int after = 0;
+// 	int before = 0;
 //     (void)ac;
-// 	printf("1\n");
 //     while(av[i])
 //     {
+// 		printf("saisyomoziretu:%s\n",av[i]);
 //         while(av[i][j] != '$' && av[i][j])
 //             j++;
 // 		// printf("%s\n",av[i]);
 // 		// printf("%c\n",av[i][j]);
+// 		int saisyo = j;
 // 		if(av[i][j] == '$' && av[i][j])
 // 		{
 //         	j++;
-//         	printf("%s\n",tenkai(av[i],ev,"exit_status",j));
-// 			j++;
+// 			tenkai_no_nakami = tenkai(av[i],ev,"exit_status",j,&ev_len);
+//         	printf("tenkai:%s\n",tenkai_no_nakami);
+// 			before = ev_len;
+// 			after = strlen(tenkai_no_nakami);
+// 			while(after)
+// 				(void)(after--,j++);
 // 		}
 // 		else
 // 		{
 //         	i++;
 // 			j = 0;
-// 			printf("3\n");
+// 			printf("owari\n");
+// 			if(!av[i])
+// 				break;
 // 		}
-//     }
+// 		char *res = NULL;
+// 		res = ft_substr(av[i],0,saisyo);//saisyo
+// 		printf("saisyo:%s\n",res);
+// 		res = ft_strjoin(res,tenkai_no_nakami);//nakami
+// 		res = ft_strjoin(res,ft_substr(av[i],saisyo + before +1,strlen(av[i]) - before+1 + after));//saigo
+// 		printf("saigo:%s\n",res);
+// 		av[i] = strdup(res);
+// 	}
 //     return 0;
 // }
