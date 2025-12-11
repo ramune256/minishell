8e643d791c7230854e1bf3de5b41e37218ceed7a
nmasu <nanaho.masuda.19@gmail.com>
2025-12-11 19:44:50 +0900

    fix commentout

--- srcs/core_expander.c
+++ srcs/core_expander.c
@@ -86,3 +86,85 @@ void expander(t_cmd *ast, t_alloc *heap)
     if(ast->right)
        expander(ast->right,heap);
 }
+
+
+/*
+int	main(int ac, char **av, char **ev)
+{
+	(void)ac;
+	for (int i = 1; av[i]; i++)
+	{
+		char *cur = strdup(av[i]);
+		if (!cur)
+			return (1);
+		printf("saisyomoziretu:%s\n", cur);
+		int j = 0;
+		while (1)
+		{
+			while (cur[j] && cur[j] != '$')
+				j++;
+			if (!cur[j])
+			{
+				printf("owari\n");
+				break;
+			}
+			int saisyo = j;
+			j++;
+			int ev_len = 0;
+			char *tenkai_no_nakami = tenkai(cur, ev, "exit_status", j, &ev_len);
+			if(tenkai_no_nakami)
+			{
+				int before = ev_len;//変数の展開前の長さ
+				int after = (int)strlen(tenkai_no_nakami);//変数の展開後の長さ	
+
+				//prefix →　変数前の文字列
+				char *prefix = ft_substr(cur, 0, (size_t)saisyo);
+				if (!prefix)
+				{ free(tenkai_no_nakami); free(cur); return (1); }
+
+				//prefix＋展開後の変数
+				char *tmp = ft_strjoin(prefix, tenkai_no_nakami);
+				free(prefix);
+				if (!tmp) { free(tenkai_no_nakami); free(cur); return (1); }
+
+				int tail_start = saisyo + 1 + before;
+				int cur_len = strlen(cur);
+				int tail_len = 0;
+				if (tail_start <= cur_len)
+					tail_len = cur_len - tail_start;
+				else
+					tail_len = 0;
+
+				char *tail = ft_substr(cur, (unsigned int)tail_start, tail_len);
+				if (!tail)
+				{ free(tmp); free(tenkai_no_nakami); free(cur); return (1); }
+
+				//res = tmp(prefix + 展開後)+tail
+				char *res = ft_strjoin(tmp, tail);
+				free(tmp);
+				free(tail);
+				if (!res) { free(tenkai_no_nakami); free(cur); return (1); }
+
+				printf("saigo:%s\n", res);
+				free(cur);
+				cur = strdup(res);
+				free(res);
+				free(tenkai_no_nakami);
+				if (!cur)
+					return (1);
+				j = saisyo + after;
+				if (j < 0)
+					j = 0;
+				if (j > strlen(cur))
+					j = strlen(cur);
+				printf("saisyomoziretu:%s\n", cur);
+			}
+			else
+				j = saisyo + 1;
+		}
+		av[i] = cur;
+	}
+	return (0);
+}
+
+*/
