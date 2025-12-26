25f7af226a1bbd13c57f6982cb5e083df8c8f367
ramune256 <shungo180620@outlook.jp>
2025-12-27 01:35:05 +0900

    fix echo

--- srcs/builtin/builtin_echo.c
+++ srcs/builtin/builtin_echo.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_echo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmasuda <nmasuda@student.42.fr>            +#+  +:+       +#+        */
+/*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 17:43:26 by nmasuda           #+#    #+#             */
-/*   Updated: 2025/12/27 01:04:26 by nmasuda          ###   ########.fr       */
+/*   Updated: 2025/12/27 01:34:53 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	echo_print(char **line)
+static bool	echo_print(const char **line)
 {
 	int	i;
 
-	i = CMD + 1;
+	i = 0;
 	while (line[i])
 	{
 		if (printf("%s", line[i++]) == ERROR)
@@ -30,40 +30,43 @@ static bool	echo_print(char **line)
 	return (true);
 }
 
-static bool	is_n(char *str)
+static bool	is_n_opt(const char *arg)
 {
 	int	i;
 
-	if (!str || ft_strncmp(str, "-n", 2))
+	if (!arg || ft_strncmp(arg, "-n", 2))
 		return (false);
-	i = 2;
-	while (str[i] == 'n')
+	i = ft_strlen("-n");
+	while (arg[i] == 'n')
 		i++;
-	if (!str[i])
-		return (true);
-	return (false);
+	if (arg[i] != '\0')
+		return (false);
+	return (true);
 }
 
 int	c_echo(char **line, t_alloc *heap)
 {
+	int	i;
+
+	i = 1;
 	(void)heap;
 	if (!line)
 		return (1);
-	if (!line[OPT])
+	if (!line[1])
 	{
 		if (printf("\n") == ERROR)
 			return (1);
 	}
-	else if (is_n(line[OPT]))
+	else if (is_n_opt(line[1]))
 	{
-		while (line[OPT] && is_n(line[OPT]))
-			line++;
-		if (echo_print(line) == false)
+		while (line[i] && is_n_opt(line[i]))
+			i++;
+		if (echo_print(line + i) == false)
 			return (1);
 	}
 	else
 	{
-		if (echo_print(line) == false)
+		if (echo_print(line + 1) == false)
 			return (1);
 		if (printf("\n") == ERROR)
 			return (1);
