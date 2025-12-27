209cad6048fb7cbd77531f247e41fa28a0ae03d9
nmasu <nanaho.masuda.19@gmail.com>
2025-12-28 01:51:03 +0900

    add: cntrl-c

--- srcs/core_executor.c
+++ srcs/core_executor.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   core_executor.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: nmasuda <nmasuda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 21:10:31 by shunwata          #+#    #+#             */
-/*   Updated: 2025/12/21 16:23:35 by shunwata         ###   ########.fr       */
+/*   Updated: 2025/12/28 01:50:05 by nmasuda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	branch_pipe_or_child_task(t_cmd *node, t_alloc *heap);
 
 static t_cmd	*handle_redirections(t_cmd *ast, t_alloc *heap)
 {
@@ -29,11 +31,25 @@ static t_cmd	*handle_redirections(t_cmd *ast, t_alloc *heap)
 	return (exec_node);
 }
 
+void	execute_child_task(t_cmd *node, t_alloc *heap)
+{
+	t_cmd	*exec_node;
+	char	*fullpath;
+
+	set_signal_child();
+	exec_node = handle_redirections(node, heap);
+	if (execute_builtin(exec_node, heap))
+		(cleanup(heap), exit(heap->exit_status));
+	fullpath = get_fullpath(exec_node->argv[0], heap);
+	if (fullpath == NULL)
+		(cleanup(heap), exit(127));
+	if (execve(fullpath, exec_node->argv, heap->ev_clone) == -1)
+		(perror(fullpath), free(fullpath), cleanup(heap), exit(126));
+}
+
 static void	execute_single_command(t_cmd *ast, t_alloc *heap)
 {
 	pid_t	pid;
-	t_cmd	*exec_node;
-	char	*fullpath;
 	int		status;
 
 	if (ast->type != NODE_PIPE)
@@ -42,17 +58,7 @@ static void	execute_single_command(t_cmd *ast, t_alloc *heap)
 	if (pid == -1)
 		(perror("fork"), cleanup(heap), exit(1));
 	if (pid == 0)
-	{
-		set_signal_child();
-		exec_node = handle_redirections(ast, heap);
-		if (execute_builtin(exec_node, heap))
-			(cleanup(heap), exit(heap->exit_status));
-		fullpath = get_fullpath(exec_node->argv[0], heap);
-		if (fullpath == NULL)
-			(cleanup(heap), exit(127));
-		if (execve(fullpath, exec_node->argv, heap->ev_clone) == -1)
-			(perror(fullpath), free(fullpath), cleanup(heap), exit(126));
-	}
+		execute_child_task(ast, heap);
 	set_signal_parent();
 	waitpid(pid, &status, 0);
 	set_signal_shell();
@@ -66,10 +72,10 @@ static void	execute_single_command(t_cmd *ast, t_alloc *heap)
 
 static void	execute_pipe(t_cmd *ast, t_alloc *heap)
 {
+	int		status;
 	int		pipefd[2]; // [0]は読み込み口, [1]は書き込み口
 	pid_t	pid_left;
 	pid_t	pid_right;
-	int		status;
 
 	if (pipe(pipefd) == -1)
 		(perror("pipe"), cleanup(heap), exit(1)); // パイプ作成失敗は致命的
@@ -81,7 +87,7 @@ static void	execute_pipe(t_cmd *ast, t_alloc *heap)
 		set_signal_child();
 		dup2(pipefd[1], STDOUT_FILENO);
 		(close(pipefd[0]), close(pipefd[1]));
-		execute(ast->left, heap); // 左側のASTを再帰的に実行
+		branch_pipe_or_child_task(ast->left, heap);
 		(cleanup(heap), exit(heap->exit_status)); // 子プロセスをクリーンアップ
 	}
 	pid_right = fork(); // 2. 右側の子プロセス (パイプから読み込む側)
@@ -92,7 +98,7 @@ static void	execute_pipe(t_cmd *ast, t_alloc *heap)
 		set_signal_child();
 		dup2(pipefd[0], STDIN_FILENO);
 		(close(pipefd[0]), close(pipefd[1]));
-		execute(ast->right, heap); // 右側のASTを再帰的に実行
+		branch_pipe_or_child_task(ast->right, heap);
 		(cleanup(heap), exit(heap->exit_status)); // 子プロセスをクリーンアップ
 	}
 	(close(pipefd[0]), close(pipefd[1]));
@@ -100,6 +106,16 @@ static void	execute_pipe(t_cmd *ast, t_alloc *heap)
 	(waitpid(pid_left, NULL, 0), waitpid(pid_right, &status, 0));
 	set_signal_shell();
 	get_exit_status(heap, status);
+}
+
+void	branch_pipe_or_child_task(t_cmd *node, t_alloc *heap)
+{
+	if (!node)
+		return ;
+	if (node->type == NODE_PIPE)
+		execute_pipe(node, heap);
+	else
+		execute_child_task(node, heap);
 }
 
 // ASTを再帰的にたどって実行するエントリーポイント
