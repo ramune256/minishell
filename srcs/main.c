c0395ef79a2f4ae569d8f65da8b5516e8ffe21bd
ramune256 <shungo180620@outlook.jp>
2025-12-28 20:03:21 +0900

    fix main.c

--- srcs/main.c
+++ srcs/main.c
@@ -3,14 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmasuda <nmasuda@student.42.fr>            +#+  +:+       +#+        */
+/*   By: shunwata <shunwata@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 21:10:07 by shunwata          #+#    #+#             */
-/*   Updated: 2025/12/28 01:49:19 by nmasuda          ###   ########.fr       */
+/*   Updated: 2025/12/28 20:03:09 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	import_signal_status(t_alloc *heap)
+{
+	if (g_sig_status)
+	{
+		heap->exit_status = 128 + SIGINT;
+		g_sig_status = 0;
+	}
+}
+
+static void	interpret(t_alloc *heap)
+{
+	if (isatty(STDIN_FILENO))
+		add_history(heap->line);
+	tokenize(heap);
+	parse(heap);
+	expand(heap->ast, heap);
+	print_ast(heap->ast, 0);
+	execute(heap->ast, heap);
+}
 
 int	main(int ac, char **av, char **ev)
 {
@@ -23,20 +43,9 @@ int	main(int ac, char **av, char **ev)
 	while (1)
 	{
 		get_input(&heap.line, "minishell> ");
-		if (g_sig_status)
-		{
-			heap.exit_status = 130;
-			g_sig_status = 0;
-		}
+		import_signal_status(&heap);
 		if (heap.line)
-		{
-			if (isatty(STDIN_FILENO))
-				add_history(heap.line);
-			tokenize(&heap);
-			parse(&heap);
-			expand(heap.ast, &heap);
-			execute(heap.ast, &heap);
-		}
+			interpret(&heap);
 		else
 			print_exit(&heap);
 		heap.success = true;
